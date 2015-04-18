e4d1467afce3ae76a7e3fc1d44906b1e0c88737b
Nander Voortman <nander@nander.org>
2015-04-18 11:07:46 +0200

    initial commit

---
+++ gamestate.lua
@@ -0,0 +1,16 @@
+local bump       = require 'bump'
+local bump_debug = require 'bump_debug'
+require 'mapcol'
+local sti = require "Simple-Tiled-Implementation"
+
+function resetgamestate(mname)
+
+    local s = {}
+    s.scroll = 200
+    s.player = { x=50,y=50,w=32,h=32,r=0, speed = 300 }
+    s.blocks = {}
+    s.world = bump.newWorld()
+    s.map = sti.new("maps/testmap2")
+    s.collisiontiles = findSolidTiles(s)
+    return s
+end
