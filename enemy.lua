86974e5a7319735b95b5c637b70e7a83c856f175
Nander Voortman <nander@nander.org>
2015-04-18 14:32:42 +0200

    a lot added

--- enemy.lua
+++ enemy.lua
@@ -1,22 +1,21 @@
 function findEnemies(gamestate)
   local layer = gamestate.map.layers["enemies"]
+  local map = gamestate.map
   local enemies = {}
   local o = layer.objects
 		for _, v in ipairs(o) do
       if  v then
-        props = v.properties
+        props =v.properties
         local type = ""
         print(#v.properties)
-        for d, p in ipairs(props) do
-          if d== "type" then
-            v.type = p
-          end
-
-          if d== "ctype" then
-            v.ctype = p
-            print(p)
-          end
+        local ctype = v.properties.ctype
+        if ctype then
+          --v.ctype = ctype
         end
+        local type = v.properties.type
+        if type then
+            v.type = type
+          end
         v.w = v.width
         v.h = v.height
         gamestate.world:add(v,v.x,v.y,v.width,v.height)
@@ -25,20 +24,39 @@ function findEnemies(gamestate)
         enemies[#enemies+1] = v
         gamestate.blocks[#gamestate.blocks+1] = v
       end
-
 		end
     return enemies
-
 end
 
-function aienemy(gamestate,dt)
+function aienemy(dt)
   --print("enemy")
 
   for _,enemy in ipairs(gamestate.enemies) do
-    dx = 10
-    dy = 0
-    enemy.x, enemy.y, cols, cols_len =gamestate.world:move(enemy, enemy.x + dx*dt, enemy.y + dy*dt)
+    if -gamestate.scroll < tonumber(enemy.properties.scroll) then
+
+      laneAI(enemy,dt)
+    end
     if cols_len > 0 then
     end
   end
+end
+
+function laneAI(enemy,dt)
+  local velocity = 100
+  local r = math.rad(enemy.rotation)
+  local dx = math.cos(r)* velocity * dt
+  local dy = math.sin(r)*velocity*dt
+
+  enemy.x, enemy.y, cols, cols_len =gamestate.world:move(enemy, enemy.x + dx, enemy.y + dy,enemyfilter)
+  enemy.properties.tick = enemy.properties.tick + dt
+  if  enemy.properties.tick > 0.5 then
+    enemy.properties.tick = 0
+    print("shooting")
+  end
+end
+
+function enemyfilter(item, other)
+  return 'cross'
+
+  -- else return nil
 end
--- gamestate.lua
+++ gamestate.lua
@@ -5,16 +5,25 @@ require 'enemy'
 local sti = require "Simple-Tiled-Implementation"
 
 function resetgamestate(mname)
-
     local s = {}
-
-    s.player = { x=50,y=50,w=8,h=8,r=0, speed = 450,xoffset = 28,yoffset = 4 }
+    print(mname)
+    s.player = { x=love.graphics.getWidth()/4,y=2,w=8,h=8,r=0, speed = 450,xoffset = 28,yoffset = 4 }
     s.blocks = {}
     s.world = bump.newWorld()
-    s.map = sti.new("maps/testmap2")
+    s.mapname = mname
+    s.map = sti.new(mname)
     s.collisiontiles = findSolidTiles(s)
-    s.scroll = -s.map.height*tile_height
+    s.scroll = -s.map.height*tile_height +love.graphics.getHeight()*1.5
     s.player.y = s.player.y - s.scroll
     s.enemies = findEnemies(s)
+    s.endStates = findEndLevelTiles(s)
     return s
+end
+function reload_upon_death()
+  print("your mom")
+  local s = resetgamestate(gamestate.mapname)
+  s.player.y = s.player.y + s.scroll
+  s.scroll = -s.map.height*tile_height +love.graphics.getHeight()*0.5
+  s.player.y = s.player.y - s.scroll
+  return s
 end
